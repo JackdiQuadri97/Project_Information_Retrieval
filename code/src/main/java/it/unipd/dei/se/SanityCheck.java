133357bd24d89a738dfc63c0ecd49c191545d35b
munzerazra <munzerazra11@gmail.com>
2022-04-08 15:32:32 +0200

    fix: edited the files links

--- code/src/main/java/it/unipd/dei/se/SanityCheck.java
+++ code/src/main/java/it/unipd/dei/se/SanityCheck.java
@@ -14,7 +14,7 @@ import java.nio.file.Path;
 public class SanityCheck {
     public static void main(String[] args) throws IOException {
         System.out.println("Testing document parser");
-        DocumentParser dp = DocumentParser.create(Parser.class, Files.newBufferedReader(Path.of("code/src/main/resources/test-expanded.jsonl"), StandardCharsets.ISO_8859_1));
+        DocumentParser dp = DocumentParser.create(Parser.class, Files.newBufferedReader(Path.of("code/src/main/resource/test-expanded.jsonl"), StandardCharsets.ISO_8859_1));
         if (dp.hasNext()) {
             ParsedDocument parsedDocument = dp.next();
             System.out.println("Parsed document:");
@@ -24,7 +24,7 @@ public class SanityCheck {
         }
 
         System.out.println("Testing topic parser");
-        XMLTopicParser tp = new XMLTopicParser(Files.newBufferedReader(Path.of("code/src/main/resources/topics-task2.xml"), StandardCharsets.ISO_8859_1));
+        XMLTopicParser tp = new XMLTopicParser(Files.newBufferedReader(Path.of("code/src/main/resource/topics-task2.xml"), StandardCharsets.ISO_8859_1));
         if (tp.hasNext()) {
             ParsedTopic parsedTopic = tp.next();
             System.out.println("Parsed topic:");
